a73954dc2723ef16c0bd2d76da2dc6530816bdc5
iqun <805632731@qq.com>
2020-02-23 22:43:29 +0800

    easy excel使用

--- pinellia-service/pinellia-user/src/main/java/com/ivan/pinellia/UserApplication.java
+++ pinellia-service/pinellia-user/src/main/java/com/ivan/pinellia/UserApplication.java
@@ -1,7 +1,11 @@
 package com.ivan.pinellia;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -29,4 +33,21 @@ public class UserApplication {
         SpringApplication.run(UserApplication.class);
     }
 
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
+
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                System.out.println("Number of process definitions : "
+                        + repositoryService.createProcessDefinitionQuery().count());
+                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+                System.out.println("Number of tasks after process start: "
+                        + taskService.createTaskQuery().count());
+            }
+        };
+    }
 }
