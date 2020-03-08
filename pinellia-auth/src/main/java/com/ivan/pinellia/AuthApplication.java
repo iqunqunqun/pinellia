b0c9e2d6a48f76bd588359b7d4e2e943533c6b2c
iqun <805632731@qq.com>
2020-03-08 22:46:38 +0800

    kafka 使用

--- pinellia-auth/src/main/java/com/ivan/pinellia/AuthApplication.java
+++ pinellia-auth/src/main/java/com/ivan/pinellia/AuthApplication.java
@@ -2,6 +2,8 @@ package com.ivan.pinellia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author chenyf
@@ -10,7 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @create 2020-01-12 2:12 下午
  */
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class);
--- pinellia-service/pinellia-user/src/main/java/com/ivan/pinellia/UserApplication.java
+++ pinellia-service/pinellia-user/src/main/java/com/ivan/pinellia/UserApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 @MapperScan("com.ivan.pinellia.mapper")
 public class UserApplication {
 
